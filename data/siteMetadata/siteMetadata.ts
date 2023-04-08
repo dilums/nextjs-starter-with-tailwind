b13425c5e723941757c684670ca5bf53dfc71c68
dilums <dilumsanjaya@gmail.com>
2023-04-08 08:14:40 +0530

    initial commit

---
+++ data/siteMetadata/siteMetadata.ts
@@ -0,0 +1,12 @@
+const siteMetadata = {
+  title: "",
+  description: "",
+  ogImage: "/og.png",
+  url: "https://dilums.com",
+  author: "Dilum Sanjaya",
+  keywords: "Next.js",
+  favicon: "/favicon.ico",
+  language: "en-US",
+};
+
+export default siteMetadata;
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import CustomHead from "@/components/common/CustomHead";
+
+export default function Home() {
+  return (
+    <>
+      <CustomHead title="Home" />
+      <div className="max-w-7xl mx-auto p-4">Home</div>
+    </>
+  );
+}
